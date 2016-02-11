14d8f44e5a9552756272a1365ee01ae20d4b3822
Joshua Sarver <jks1010@sru.edu>
2016-02-11 01:58:08 -0500

    started work on chapter six;

---
+++ Learn You a Haskell/Chapter 06.hsproj/Chapter_06.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+import Prelude
+import Data.List
+import Data.Char
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+
+wordNums :: String -> [(String,Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+
+isInfixOf' :: Eq a => [a] -> [a] -> Bool
+n `isInfixOf'` h = any (n `isPrefixOf`) (tails h)
+
+
+encode :: Int -> [Char] -> [Char]
+--encode n s = map chr . map (+n) . map ord $ s
+encode offset msg = map (chr . (+offset) . ord) $ msg
+
+
+decode :: Int -> [Char] -> [Char]
+decode offset msg = encode (negate offset) msg
+
+
+
+firstToN n = find (isSumN) [1..]
+              where isSumN = (== n) . digitSum
+              
+firstToForty = firstToN 40
+
+
+
+                    
+digitSum :: Int -> Int
+digitSum = sum . map (digitToInt) . show
+
