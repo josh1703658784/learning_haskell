6d7d952e01e87b984e4237984c06f279a05f8ebc
Joshua Sarver <jks1010@sru.edu>
2016-02-12 14:35:29 -0500

    working with modules; the IDE was having some bug with recognizing syntax...

--- Learn You a Haskell/Chapter 06.hsproj/Chapter_06.hs
+++ Learn You a Haskell/Chapter 06.hsproj/Chapter_06.hs
@@ -2,6 +2,7 @@
 import Prelude
 import Data.List
 import Data.Char
+import Geometry
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -30,9 +31,22 @@ firstToN n = find (isSumN) [1..]
               
 firstToForty = firstToN 40
 
-
-
                     
 digitSum :: Int -> Int
 digitSum = sum . map (digitToInt) . show
+
+
+phoneBook = 
+    [("betty","555-2938")    ,("betty","342-2492")    ,("bonnie","452-2928")
+    ,("patsy","493-2928")    ,("patsy","943-2929")    ,("patsy","827-9162")    ,("lucille","205-2928")    ,("wendy","939-8282")    ,("penny","853-2492")    ,("penny","555-2111")    ]
+    
+--
+--phonebookToMap :: (Ord k) => [(k, String)] -> M.Map k String
+--phonebookToMap xs = M.fromListWith add xs
+--                  where add num1 num2 = num1 ++ ", " ++ num2
+--
+--
+
+
+
 
