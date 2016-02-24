33af478caf89fc5aa4e91a73ac67fc3856c12c29
Joshua Sarver <jks1010@sru.edu>
2016-02-24 01:29:25 -0500

    phew, long chapter...

--- Learn You a Haskell/Chapter 07.hsproj/Chapter_07.hs
+++ Learn You a Haskell/Chapter 07.hsproj/Chapter_07.hs
@@ -1,21 +1,42 @@
+--PAGE 129
 import Shapes
-
+data Vector a = Vector a a a deriving (Show)
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday           deriving (Eq, Ord, Show, Read, Bounded, Enum)
 data Person = Person { firstName :: String
                      , lastname  :: String 
                      , age       :: Int
                      , height    :: Float
                      , phoneNumber :: String
                      , flavor    :: String 
-                     } deriving (Show)
+                     } deriving (Show, Eq, Read)
               
+type Number = String
+type Name = String
+type Phonebook = [(Name, Number)]
 
---PAGE 117
+phonebook :: Phonebook
+phonebook = [("josh", "7246460485")
+            ,("tesla", "8147044032")
+            ]
+            
+inPhonebook :: Name -> Number -> Bool
+inPhonebook name num = (name, num) `elem` phonebook
+
+tellPerson :: Person -> String
+tellPerson p = "I'm "++firstName p++" and I am "++show (age p)++" years old!"
 
 
+vPlus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector a b c) `vPlus` (Vector x y z) = Vector (a+x) (b+y) (c+z)
 
+vMult :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector a b c) `vMult` (Vector x y z) = Vector (a*x) (b*y) (c*z)
 
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector a b c) `dotProd` (Vector x y z) = (a*x)+(b*y)+(c*z)
 
-
+--
+--
 --firstName :: Person -> String
 --firstName (Person fn _ _ _ _ _) = fn
 --
@@ -33,3 +54,5 @@ data Person = Person { firstName :: String
 --
 --flavor :: Person -> String
 --flavor (Person _ _ _ _ _ i) = i
+
+
